272093800e009dce4b8bcb3f10871d4d824a3fe0
Martin Slováček <ms250918@ncr.com>
2019-11-01 14:20:12 +0100

    Added a barebones project

---
+++ UiAutomationDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using FlaUI.UIA3;
+using FlaUI.Core.Input;
+using FlaUI.Core;
+using System;
+using System.Threading;
+
+namespace UiAutomationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = Application.Launch("notepad");
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                var menu = window.FindFirstDescendant(cf => cf.ByAutomationId("MenuBar")).AsMenu();
+                menu.Items[0].Click();
+
+                Thread.Sleep(2000);
+                app.Close();
+            }
+
+        }
+    }
+}
