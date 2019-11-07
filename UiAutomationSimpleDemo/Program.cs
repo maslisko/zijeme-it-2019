c43ea7ceed83c458cc3f565fe57f63ccbe24551b
Martin Slováček <ms250918@ncr.com>
2019-11-07 12:12:24 +0100

    Completed simple demo (originally purposedly empty project)

--- UiAutomationSimpleDemo/Program.cs
+++ UiAutomationSimpleDemo/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlaUI.UIA3;
+using FlaUI.Core;
+
+
 
 namespace UiAutomationSimpleDemo
 {
@@ -10,6 +14,24 @@ namespace UiAutomationSimpleDemo
     {
         static void Main(string[] args)
         {
+            var app = Application.LaunchStoreApp("Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            using (var automation = new UIA3Automation())
+            {
+                var window = automation.GetDesktop().FindFirstDescendant(cf => cf.ByName("Kalkulačka"));
+                // var window = app.GetMainWindow(automation);
+                window.SetForeground();
+                var buttonPi = window.FindFirstDescendant(cf => cf.ByAutomationId("piButton"));
+                buttonPi.Click();
+
+                var resultBox = window.FindFirstDescendant(cf => cf.ByAutomationId("CalculatorResults"));
+                var result = resultBox.Name;
+                result = result.Replace("Zobrazuje se ", string.Empty);
+
+                Console.WriteLine(result);
+                Console.ReadKey();
+
+                app.Close();
+            }
         }
     }
 }
